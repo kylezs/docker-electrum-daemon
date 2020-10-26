0f8c9f2396f5966309f196d7204225421db4183a
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-10-26 15:35:00 +1100

    Testnet flag read correctly on docker-compose up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,21 +5,21 @@ set -ex
 trap 'pkill -TERM -P1; electrum daemon stop; exit 0' SIGTERM
 
 # Testnet support
-if [ "$TESTNET" = true ]; then
+if [ ${TESTNET} = true ]; then
   FLAGS='--testnet'
 fi
 
 # Run application
-electrum daemon -d --testnet
+electrum daemon -d $FLAGS
 
 # get some info, test if daemon is up
-electrum getinfo --testnet
+electrum getinfo $FLAGS
 
 # Set config
-electrum setconfig rpcuser ${ELECTRUM_USER} --testnet
-electrum setconfig rpcpassword ${ELECTRUM_PASSWORD} --testnet
-electrum setconfig rpchost 0.0.0.0 --testnet
-electrum setconfig rpcport 7777 --testnet
+electrum setconfig rpcuser ${ELECTRUM_USER} $FLAGS
+electrum setconfig rpcpassword ${ELECTRUM_PASSWORD} $FLAGS
+electrum setconfig rpchost 0.0.0.0 $FLAGS
+electrum setconfig rpcport 7777 $FLAGS
 
 # XXX: Check load wallet or create
 
